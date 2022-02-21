f26d7f84946920fea01fb996ba25c3425817a2be
Gökçer <61627246+gokcererol@users.noreply.github.com>
2022-02-21 20:26:52 +0300

    Add files via upload

---
+++ Lesson1 Wrap Up.m
@@ -0,0 +1,9 @@
+speed_mps=100/9.58;
+speed_kph=3.6*speed_mps;
+hundred=speed_kph;
+disp(hundred);
+
+hour=2+1/60+39/3600;
+km=42.195;
+marathon=km/hour;
+disp(marathon);
---
+++ MATLAB as a Calculator.m
@@ -0,0 +1,4 @@
+initial = 1000;
+rate = 0.1;
+debt = initial * (1 + rate) ^ 2;
+disp(debt);
